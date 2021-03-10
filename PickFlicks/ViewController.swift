64b699db67be5e156e00858be94802adf067fce3
Paul Bunnell <bunnellrpaul@gmail.com>
2021-03-10 09:47:24 -0700

    Test Change
    Added print(“New thing”)

--- PickFlicks/ViewController.swift
+++ PickFlicks/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("Whatever i want")
+        print("New thing")
     }
 
 
