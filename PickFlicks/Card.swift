34e42a7c288b63392ba85adc464f63062e7f88f5
Ryan A. Anderson <ryan.anderson9921@stu.mtec.edu>
2021-03-12 10:22:04 -0700

    Finsished Draggable  View

---
+++ PickFlicks/Card.swift
@@ -0,0 +1,8 @@
+//
+//  Card.swift
+//  PickFlicks
+//
+//  Created by Ryan Anderson on 3/12/21.
+//
+
+import Foundation
---
+++ PickFlicks/MovieSwipePicker.swift
@@ -0,0 +1,20 @@
+//
+//  MovieSwipePicker.swift
+//  PickFlicks
+//
+//  Created by Ryan Anderson on 3/11/21.
+//
+
+import SwiftUI
+
+struct MovieSwipePicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovieSwipePicker_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieSwipePicker()
+    }
+}
